a741ed7e6d87c02f26f157823020868629fafc21
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-08-16 11:12:34 +0530

    component chat updated

--- src/components/Layouts/Chat/Chat.jsx
+++ src/components/Layouts/Chat/Chat.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import socket from './socket';
 import { addMessage } from './chatSlice';
+import MainPagesHeader from '../../Common/MainPagesHeader/MainPagesHeader';
 
 const Chat = () => {
   const { register, handleSubmit, reset } = useForm();
@@ -31,21 +32,34 @@ const Chat = () => {
   };
 
   return (
-    <div>
-      <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register('message')}
-          autoComplete="off"
-          placeholder="Type your message here"
-        />
-        <button type="submit">Send</button>
-      </form>
-    </div>
+    <>
+      <div className="container-fluid home-layout-div">
+        <MainPagesHeader />
+        <div className="row home-contents">
+          <div className="col-md-4">
+            <h3>Chats</h3>
+          </div>
+          <div className="col-md-8">
+            <div className="row"></div>
+            <div className="row">
+              <ul>
+                {messages.map((msg, index) => (
+                  <li key={index}>{msg}</li>
+                ))}
+              </ul>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  {...register("message")}
+                  autoComplete="off"
+                  placeholder="Type your message here"
+                />
+                <button type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
