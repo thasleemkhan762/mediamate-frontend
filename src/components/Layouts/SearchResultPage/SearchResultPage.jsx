898518ec5ca494152e8c08fa6c2c89c02efe23db
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-09-19 12:57:30 +0530

    correction

--- src/components/Layouts/SearchResultPage/SearchResultPage.jsx
+++ src/components/Layouts/SearchResultPage/SearchResultPage.jsx
@@ -41,7 +41,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -82,7 +82,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -123,7 +123,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -164,7 +164,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -205,7 +205,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -246,7 +246,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -287,7 +287,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -328,7 +328,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -369,7 +369,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -410,7 +410,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -451,7 +451,7 @@ function SearchResultPage() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
--- src/components/Layouts/UserFriends/UserFriends.jsx
+++ src/components/Layouts/UserFriends/UserFriends.jsx
@@ -194,7 +194,7 @@ function UserFriends() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -235,7 +235,7 @@ function UserFriends() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -276,7 +276,7 @@ function UserFriends() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -317,7 +317,7 @@ function UserFriends() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -358,7 +358,7 @@ function UserFriends() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
@@ -399,7 +399,7 @@ function UserFriends() {
                         >
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
                           />
                         </svg>
